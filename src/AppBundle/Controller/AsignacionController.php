1ef9e278a26e946fb241f5c253f5e728c69fba32
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-08-06 20:38:00 -0600

    Mostrar solo los cursos que no se han asignado los usuarios

--- src/AppBundle/Controller/AsignacionController.php
+++ src/AppBundle/Controller/AsignacionController.php
@@ -7,27 +7,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 use AppBundle\Entity\Curso;
 use UserBundle\Entity\Usuario;
-use  Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections;
 
 class AsignacionController extends Controller
 {
     /**
      * Don't forget to add this route annotation!
      *
-     * @Route("/listarCursos/", name="asignacion")
+     * @Route("{usuario_id}/asignar/cursos", name="asignacion")
+     * @ParamConverter("usuario", class="UserBundle:Usuario", options={"id"="usuario_id"})
      * 
      */
-    public function listarAction(Request $request)
+    public function listarAction(Request $request, Usuario $usuario)
     {
         $em = $this->getDoctrine()->getManager();
-
         $cursos = $em->getRepository('AppBundle:Curso')->findAll();
 
-		return $this->render('AppBundle:Asignacion:listar.html.twig', array('cursos' => $cursos));
+        $cursosAsignados = $usuario->getCursos();
+
+        $returnData = [];
+
+        foreach($cursos as $curso){       
+            if (!$cursosAsignados->contains($curso)) {
+                $returnData[] = $curso;
+            }
+        } 
+        
+		return $this->render('AppBundle:Asignacion:asignar.html.twig', array('cursos' => $returnData));
     }
 
     /**
-     * Don't forget to add this route annotation!
+     * Método para asingar un curso a un usuario
      *
      * @Route("/listarCursos/agregar/{usuario_id}/{curso_id}/", name="add_asignacion")
      * @ParamConverter("usuario", class="UserBundle:Usuario", options={"id"="usuario_id"})
@@ -37,8 +49,8 @@ class AsignacionController extends Controller
     	$em = $this->getDoctrine()->getManager();
     	$cursos = $em->getRepository('AppBundle:Curso')->findAll();
     	$usuario->addCurso($curso);
-            $em->persist($usuario);
-            $em->flush();
+        $em->persist($usuario);
+        $em->flush();
 
 		return $this->render('AppBundle:Asignacion:listar.html.twig', array('cursos' => $cursos,
 			'usuario' => $usuario ));
