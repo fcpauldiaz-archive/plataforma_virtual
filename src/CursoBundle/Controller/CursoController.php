79e8b6575c7c03c61181ebd10b79f61a709abd26
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-09-03 19:18:17 -0600

    intento 8

--- src/CursoBundle/Controller/CursoController.php
+++ src/CursoBundle/Controller/CursoController.php
@@ -57,7 +57,7 @@ class CursoController extends Controller
         $application->setAutoExit(false);
 
         $input = new ArrayInput(array(
-           'command' => 'fos:elastica:populate',
+           'command' => 'run fos:elastica:populate',
         ));
         // You can use NullOutput() if you don't need the output
         $output = new NullOutput();
