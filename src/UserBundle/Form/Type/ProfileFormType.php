6d9c2aee9bdac9de6cb679b8aab21ffc66058576
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-09-04 10:06:42 -0600

    actualizar formulario de perfil

--- src/UserBundle/Form/Type/ProfileFormType.php
+++ src/UserBundle/Form/Type/ProfileFormType.php
@@ -4,14 +4,41 @@ namespace UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Validator\Constraint\UserPassword as OldUserPassword;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 
 class ProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        if (class_exists('Symfony\Component\Security\Core\Validator\Constraints\UserPassword')) {
+            $constraint = new UserPassword();
+        } else {
+            // Symfony 2.1 support with the old constraint class
+            $constraint = new OldUserPassword();
+        }
+
         // agregar campos personalizados
-        $builder->add('nombreCompleto',null,array('label' => 'Nombre Completo'));
+        $builder->add('nombreCompleto',null,array('label' => false))
+                ->add('username',null,['label'=>false])
+                ->add('email','email',['label'=>false])
+                ->add('current_password', 'password', array(
+                'label' => false,
+                'translation_domain' => 'FOSUserBundle',
+                'mapped' => false,
+                'constraints' => $constraint,
+                ))
+                ->add('plainPassword', 'repeated', array(
+                'label'=>false,
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => false),
+                'second_options' => array('label' => false),
+                'invalid_message' => 'fos_user.password.mismatch',
+                ))
+                ;
+
     }
 
     public function getParent()
