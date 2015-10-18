03822f063f5fa85787806fe61cfa032900b8aa6f
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-10-17 20:10:57 -0600

    Crear comando en consola para crear cursos

---
+++ src/CursoBundle/Command/CreateCursoCommand.php
@@ -0,0 +1,86 @@
+<?php
+namespace CursoBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use CursoBundle\Entity\Curso;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
+
+
+class CreateCursoCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('curso:create')
+            ->setDescription('Create a user.')
+            ->setDefinition(array(
+                new InputArgument('nombre', InputArgument::REQUIRED, 'Nombre curso'),
+                new InputArgument('codigo', InputArgument::REQUIRED, 'Código curso'),
+            ))
+            
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $nombre = $input->getArgument('nombre');
+        $codigo = $input->getArgument('codigo');
+        $text = '';
+        if ($nombre && $codigo) {
+            $curso = new Curso();
+            $curso->setNombreCurso($nombre);
+            $curso->setCodigoCurso($codigo);
+            $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+            $em->persist($curso);
+            $em->flush();
+            $text = 'Curso '.$nombre.' creado exitosamente';
+
+           
+        } else {
+            $text = 'Error al crear el curso'.$nombre.$codigo;
+        }
+
+      
+
+        $output->writeln($text);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if (!$input->getArgument('nombre')) {
+            $nombre = $this->getHelper('dialog')->askAndValidate(
+                $output,
+                'Ingrese nombre del curso:',
+                function($nombre) {
+                    if (empty($nombre)) {
+                        throw new \Exception('El nombre no puede estar vacío');
+                    }
+
+                    return $nombre;
+                }
+            );
+            $input->setArgument('nombre', $nombre);
+        }
+        if (!$input->getArgument('codigo')) {
+            $codigo = $this->getHelper('dialog')->askAndValidate(
+                $output,
+                'Ingrese el código del curso:',
+                function($codigo) {
+                    if (empty($codigo)) {
+                        throw new \Exception('El código no puede estar vacío');
+                    }
+
+                    return $codigo;
+                }
+            );
+            $input->setArgument('codigo', $codigo);
+        }
+    }
+
+    
+}
