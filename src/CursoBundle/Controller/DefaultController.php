e98c643e220781c27b1ac130a2c935507f6ab191
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-10-25 08:46:21 -0600

    cache loading

--- src/CursoBundle/Controller/DefaultController.php
+++ src/CursoBundle/Controller/DefaultController.php
@@ -4,12 +4,15 @@ namespace CursoBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+
 
 class DefaultController extends Controller
 {
     /**
      * Default Index page.
-     *
+     * @Cache(vary={"Accept-Encoding"},expires="tomorrow")
+     * 
      * @Route("/", name="homepage")
      */
     public function indexAction()
--- web/app.php
+++ web/app.php
@@ -16,16 +16,16 @@ $apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
-$response->setVary('Accept-Encoding');
+$response->setVary(array('Accept-Encoding', 'User-Agent'));
 $response->send();
 $kernel->terminate($request, $response);
