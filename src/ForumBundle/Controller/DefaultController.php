d059e88ff03a0000f04e256fa32e1dae016b3c42
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-10-26 23:57:31 -0600

    cursos foro

--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -6,14 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+/**
+ * @Route("/foro/")
+ */
+class ForumController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/cursos")
+     * @Method("GET")
      * @Template()
      */
-    public function indexAction($name)
+    public function cursosForoAction($name)
     {
-        return array('name' => $name);
+        $em = $this->getDoctrine()->getManager();
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+        $usuario = $this->getUser();
+
+        $cursos = $usuario->getCursos();
+
+        return [
+
+        'cursos' => $cursos,
+
+        ];
     }
 }
