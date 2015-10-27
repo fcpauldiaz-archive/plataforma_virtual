97592a903fd9a5a9a46032f457c5017928487ef1
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-10-27 00:20:25 -0600

    missing use statements

--- src/ForumBundle/Controller/ForumController.php
+++ src/ForumBundle/Controller/ForumController.php
@@ -5,6 +5,7 @@ namespace ForumBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Route("/foro/")
