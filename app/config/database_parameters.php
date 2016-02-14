a0be077632574fb417152b9513c3ca7e2b385cab
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-02-14 13:04:12 -0600

    email parameters

--- app/config/database_parameters.php
+++ app/config/database_parameters.php
@@ -8,9 +8,7 @@
     $container->setParameter('database_password', getenv('MYSQL_PASSWORD'));
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'es');
-    $container->setParameter('mailer_transport', getenv('EMAIL_TRANSPORT'));
-    $container->setParameter('mailer_host', getenv('EMAIL_HOST'));
+    $container->setParameter('mailer_transport', gmail);
+    $container->setParameter('mailer_host', null);
     $container->setParameter('mailer_user', getenv('EMAL_USER'));
-    $container->setParameter('mailer_port', getenv('EMAIL_PORT'));
     $container->setParameter('mailer_password', getenv('EMAIL_PASSWORD'));
-
