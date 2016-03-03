f43dc1f8ee236ad6365420496a7992b86e03362c
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-03-03 10:19:15 -0600

    cambiar database

--- app/config/database_parameters.php
+++ app/config/database_parameters.php
@@ -1,11 +1,11 @@
 <?php
     //$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
     $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', getenv('MYSQL_HOST'));
+    $container->setParameter('database_host', getenv('DB_HOST'));
     $container->setParameter('database_port', 3306);
-    $container->setParameter('database_name', getenv('MYSQL_NAME'));
-    $container->setParameter('database_user', 'fcpauldiaz');
-    $container->setParameter('database_password', getenv('MYSQL_PASSWORD'));
+    $container->setParameter('database_name', getenv('DB_NAME'));
+    $container->setParameter('database_user', getenv('DB_USERNAME'));
+    $container->setParameter('database_password', getenv('DB_PASSWORD'));
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'es');
     $container->setParameter('mailer_transport', gmail);
